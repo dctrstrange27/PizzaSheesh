306ee66b7082224e05de36b6aeca935cc387617d
dctrrohan@gmail.com <dctrrohan@gmail.com>
2022-12-01 22:57:37 +0800

    main,login,signup layout

--- app/src/main/java/com/example/philipricedealership/MainActivity.java
+++ app/src/main/java/com/example/philipricedealership/MainActivity.java
@@ -2,13 +2,33 @@ package com.example.philipricedealership;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+
+import com.example.philipricedealership.signup.login;
+import com.example.philipricedealership.signup.signup;
 
 public class MainActivity extends AppCompatActivity {
+    Button login_btn,signup_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        login_btn = findViewById(R.id.login_btn);
+        signup_btn = findViewById(R.id.signup_btn);
+
+        login_btn.setOnClickListener(johny -> {
+            Intent toLogin = new Intent(getApplicationContext(), login.class);
+            startActivity(toLogin);
+        });
+
+        signup_btn.setOnClickListener(johny -> {
+            Intent toSignup = new Intent(getApplicationContext(), signup.class);
+            startActivity(toSignup);
+        });
+
     }
 }
---
+++ app/src/main/java/com/example/philipricedealership/signup/login.java
@@ -0,0 +1,16 @@
+package com.example.philipricedealership.signup;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.philipricedealership.R;
+
+public class login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+    }
+}
